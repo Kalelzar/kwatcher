fa277df2002a58481b0869c576dd6132f328bc49
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-05-26 15:52:02 +0300

    fix: Properly handle message metadata for replies

--- src/protocol/client-registration/event.zig
+++ src/protocol/client-registration/event.zig
@@ -2,7 +2,7 @@ const Timer = @import("../../timer.zig");
 const ClientRegistry = @import("registry.zig");
 
 pub fn announce(timer: Timer, cr: *ClientRegistry) !bool {
-    return cr.assigned_id == null and cr.state != .registered and try timer.ready("announce");
+    return cr.state != .registered and try timer.ready("announce");
 }
 
 pub fn heartbeat(timer: Timer, cr: *ClientRegistry) !bool {
