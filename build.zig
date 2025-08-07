c44b25f4394557e1fe405a9d88568e03a8dc8bf6
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-08-07 06:10:14 +0300

    fix: Refactor

--- build.zig
+++ build.zig
@@ -1,151 +1,117 @@
 const std = @import("std");
 
-const Builder = struct {
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    opt: std.builtin.OptimizeMode,
-    check_step: *std.Build.Step,
-    kwatcher: *std.Build.Module,
-    kwatcher_example: *std.Build.Module,
-    kwatcher_dump: *std.Build.Module,
-    kwatcher_test: *std.Build.Module,
-
-    fn init(b: *std.Build) Builder {
-        const target = b.standardTargetOptions(.{});
-        const opt = b.standardOptimizeOption(.{});
-
-        const check_step = b.step("check", "");
-
-        const klib = b.dependency("klib", .{ .target = target, .optimize = opt }).module("klib");
-        const rmq = b.dependency("rabbitmq_c", .{ .target = target, .optimize = opt }).artifact("rabbitmq-c-static");
-        const zamqp = b.dependency("zamqp", .{ .target = target, .optimize = opt }).module("zamqp");
-        const uuid = b.dependency("uuid", .{ .target = target, .optimize = opt }).module("uuid");
-        const metrics = b.dependency("metrics", .{ .target = target, .optimize = opt }).module("metrics");
-        const kwatcher = b.addModule("kwatcher", .{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = opt,
-        });
-        kwatcher.addImport("zamqp", zamqp);
-        kwatcher.addImport("uuid", uuid);
-        kwatcher.addImport("metrics", metrics);
-        kwatcher.addImport("klib", klib);
-        kwatcher.linkLibrary(rmq);
-        kwatcher.link_libc = true;
-
-        const kwatcher_test = b.addModule("test", .{
-            .root_source_file = b.path("src/test.zig"),
-            .target = target,
-            .optimize = opt,
-        });
-        kwatcher_test.addImport("zamqp", zamqp);
-        kwatcher_test.addImport("uuid", uuid);
-        kwatcher_test.addImport("metrics", metrics);
-        kwatcher_test.addImport("klib", klib);
-        kwatcher_test.link_libc = true;
-
-        const kwatcher_example = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = opt,
-        });
-        kwatcher_example.link_libc = true;
-        kwatcher_example.addImport("kwatcher", kwatcher);
-
-        const kwatcher_dump = b.createModule(.{
-            .root_source_file = b.path("src/tools/dmp.zig"),
-            .target = target,
-            .optimize = opt,
-        });
-        kwatcher_dump.link_libc = true;
-        kwatcher_dump.addImport("kwatcher", kwatcher);
-
-        return .{
-            .b = b,
-            .check_step = check_step,
-            .target = target,
-            .opt = opt,
-            .kwatcher = kwatcher,
-            .kwatcher_example = kwatcher_example,
-            .kwatcher_test = kwatcher_test,
-            .kwatcher_dump = kwatcher_dump,
-        };
-    }
-
-    fn addDependencies(
-        self: *Builder,
-        step: *std.Build.Step.Compile,
-    ) void {
-        _ = self;
-        step.linkLibC();
-    }
-
-    fn addExecutable(self: *Builder, name: []const u8, root_module: *std.Build.Module) *std.Build.Step.Compile {
-        return self.b.addExecutable(.{
-            .name = name,
-            .root_module = root_module,
-        });
-    }
-
-    fn addStaticLibrary(self: *Builder, name: []const u8, root_module: *std.Build.Module) *std.Build.Step.Compile {
-        return self.b.addStaticLibrary(.{
-            .name = name,
-            .root_module = root_module,
-        });
-    }
-
-    fn addTest(self: *Builder, name: []const u8, root_module: *std.Build.Module) *std.Build.Step.Compile {
-        return self.b.addTest(.{
-            .name = name,
-            .root_module = root_module,
-        });
-    }
-
-    fn installAndCheck(self: *Builder, exe: *std.Build.Step.Compile) !void {
-        const check_exe = try self.b.allocator.create(std.Build.Step.Compile);
-        check_exe.* = exe.*;
-        self.check_step.dependOn(&check_exe.step);
-        self.b.installArtifact(exe);
-    }
-};
-
 pub fn build(b: *std.Build) !void {
-    var builder = Builder.init(b);
+    // Options
+    const build_all = b.option(bool, "all", "Build all components. You can still disable individual components") orelse false;
+    const build_example = b.option(bool, "example", "Build the example application") orelse build_all;
+    const build_static_library = b.option(bool, "lib", "Build a static library object") orelse build_all;
+    const build_dump_tool = b.option(bool, "dump", "Build the dump tool") orelse build_all;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = builder.addStaticLibrary("kwatcher", builder.kwatcher);
-    builder.addDependencies(lib);
-    try builder.installAndCheck(lib);
-
-    const exe = builder.addExecutable("kwatcher-examples", builder.kwatcher_example);
-    builder.addDependencies(exe);
-    try builder.installAndCheck(exe);
-
-    const dmp = builder.addExecutable("kwatcher-dmp", builder.kwatcher_dump);
-    builder.addDependencies(dmp);
-    try builder.installAndCheck(dmp);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = builder.kwatcher_test,
-        .name = "test",
+    const kwatcher_library = b.addModule("kwatcher", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const kwatcher_example = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    const kwatcher_dump = b.createModule(.{
+        .root_source_file = b.path("src/tools/dmp.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const tests = b.addTest(.{
+        .root_module = kwatcher_library,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Artifacts:
+    const example = b.addExecutable(.{
+        .name = "kwatcher-example",
+        .root_module = kwatcher_example,
+    });
+    if (build_example) {
+        b.installArtifact(example);
+    }
+
+    const dump = b.addExecutable(.{
+        .name = "kwatcher-dmp",
+        .root_module = kwatcher_dump,
+    });
+    if (build_dump_tool) {
+        b.installArtifact(dump);
+    }
+
+    const lib = b.addStaticLibrary(.{
+        .name = "kwatcher",
+        .root_module = kwatcher_library,
+    });
+    if (build_static_library) {
+        b.installArtifact(lib);
+    }
+
+    const run_tests = b.addRunArtifact(tests);
 
     const install_docs = b.addInstallDirectory(
-        .{ .source_dir = lib.getEmittedDocs(), .install_dir = .prefix, .install_subdir = "docs" },
+        .{
+            .source_dir = lib.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs",
+        },
     );
+
+    const fmt = b.addFmt(.{
+        .paths = &.{
+            "src/",
+            "build.zig",
+            "build.zig.zon",
+        },
+        .check = true,
+    });
+
+    // Steps:
+    const check = b.step("check", "Build without generating artifacts.");
+    check.dependOn(&lib.step);
+    check.dependOn(&example.step);
+    check.dependOn(&dump.step);
+
+    const test_step = b.step("test", "Run the unit tests.");
+    test_step.dependOn(&run_tests.step);
+    // - fmt
+    const fmt_step = b.step("fmt", "Check formatting");
+    fmt_step.dependOn(&fmt.step);
+    check.dependOn(fmt_step);
+    b.getInstallStep().dependOn(fmt_step);
+    // - docs
     const docs_step = b.step("docs", "Generate docs");
     docs_step.dependOn(&install_docs.step);
     docs_step.dependOn(&lib.step);
+
+    // Dependencies:
+    // 1st Party:
+    const klib = b.dependency("klib", .{ .target = target, .optimize = optimize }).module("klib");
+    // 3rd Party:
+    const rmq = b.dependency("rabbitmq_c", .{ .target = target, .optimize = optimize }).artifact("rabbitmq-c-static");
+    const zamqp = b.dependency("zamqp", .{ .target = target, .optimize = optimize }).module("zamqp");
+    const uuid = b.dependency("uuid", .{ .target = target, .optimize = optimize }).module("uuid");
+    const metrics = b.dependency("metrics", .{ .target = target, .optimize = optimize }).module("metrics");
+    // Imports:
+    // Internal:
+    kwatcher_example.addImport("kwatcher", kwatcher_library);
+    kwatcher_dump.addImport("kwatcher", kwatcher_library);
+    // 1st Party:
+    kwatcher_library.addImport("klib", klib);
+    // 3rd Party:
+    kwatcher_library.addImport("zamqp", zamqp);
+    kwatcher_library.addImport("uuid", uuid);
+    kwatcher_library.addImport("metrics", metrics);
+    kwatcher_library.linkLibrary(rmq);
 }
